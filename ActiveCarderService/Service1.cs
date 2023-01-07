e996b18febfa71f876389993caef8782d80717c0
TharakaMadushan <madushan@perfectbss.com>
2023-01-08 02:17:47 +0530

    Add project files.

---
+++ ActiveCarderService/Service1.cs
@@ -0,0 +1,55 @@
+﻿using LeaveData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace ActiveCarderService
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        private Timer _timer = null;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Library.WriteErrorLog("Service initialize...");
+            string appsetting = ConfigurationManager.AppSettings["TimeInterval"].ToString();
+            _timer = new Timer();
+            this._timer.Interval = Convert.ToDouble(appsetting); //every 1 hour
+            this._timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            _timer.Enabled = true;
+        }
+
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                Library.WriteErrorLog("Service Started!");
+                Library.PushActiveCarderData();
+                
+            }
+            catch (Exception ex)
+            {
+                Library.WriteErrorLog(ex.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _timer.Enabled = false;
+            Library.WriteErrorLog("window service stopped");
+        }
+    }
+}
---
+++ LeaveData/Library.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace LeaveData
+{
+    public class Library
+    {
+        private HttpResponseMessage response;
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\ActiveCarderLogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static  void PushActiveCarderData()
+        {
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnFrom"].ConnectionString))
+            {
+                if (con != null)
+                {
+                    WriteErrorLog("Connection Started From DBConnFrom");
+
+                    using (var cmd = new SqlCommand("SP_HRM_GetActiveCarderData", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@RosterDate", DateTime.Now.Date);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            da.Fill(dt);
+
+                            if (dt.Rows.Count == 0)
+                            {
+                                WriteErrorLog("No Data Rows to Import");
+                            }
+                            else
+                            {
+                                WriteErrorLog("Selected : " + dt.Rows.Count + " Rows From " + con.Database + " .");
+
+                                using (var client = new HttpClient())
+                                {
+                                    client.BaseAddress = new Uri(ConfigurationManager.AppSettings["JobCostingPostingURL"].ToString());
+                                    client.DefaultRequestHeaders.Accept.Clear();
+                                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                                    var data = JsonConvert.SerializeObject(dt);
+                                    var content = new StringContent(data, Encoding.UTF8, "application/json");
+                                     client.PostAsync("api/EmployeeAvailable", content);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    WriteErrorLog("Connection didn't Established From DBConnFrom Or Job has been done UnSuccessfully!");
+                }
+            }
+        }
+    }
+}
